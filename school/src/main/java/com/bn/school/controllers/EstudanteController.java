818d4bd5d686f170afd9c87bd0a70dc071c0e8e6
VihSouza06 <visouzasantos2006@gmail.com>
2026-03-13 18:57:13 -0300

    Codigo de erro

--- school/src/main/java/com/bn/school/controllers/EstudanteController.java
+++ school/src/main/java/com/bn/school/controllers/EstudanteController.java
@@ -3,8 +3,11 @@ package com.bn.school.controllers;
 import com.bn.school.models.EstudanteModel;
 import com.bn.school.services.EstudanteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -15,19 +18,42 @@ public class EstudanteController {
     @Autowired
     private EstudanteService estudanteService;
 
-    @GetMapping
+    /* @GetMapping
     public List<EstudanteModel> findAll(){
         return estudanteService.findAll();
+    }*/
+
+    //Codigo de Erro dar 201, não 200
+    @GetMapping
+    public ResponseEntity<List<EstudanteModel>>findAll(){
+       List<EstudanteModel> requeste = estudanteService.findAll();
+       return ResponseEntity.ok().body(requeste);
     }
 
-    @PostMapping
+    /*@PostMapping
     public EstudanteModel criarEstudante(@RequestBody EstudanteModel estudanteModel){
         return estudanteService.criarEstudante(estudanteModel);
+    }*/
+
+    //Codigo de Erro dar 201, não 200
+    @PostMapping
+    public ResponseEntity<EstudanteModel> criarEstudante(@RequestBody EstudanteModel estudanteModel){
+        EstudanteModel requeste = estudanteService.criarEstudante(estudanteModel);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{id}").buildAndExpand(estudanteModel.getId()).toUri();
+        return ResponseEntity.created(uri).body(requeste);
     }
 
-    @DeleteMapping("/{id}")
+   /* @DeleteMapping("/{id}")
     public void deletarEstudante(@PathVariable Long id){
         estudanteService.deletarEstudante(id);
+    }*/
+
+    //Codigo de Erro dar 204, não 200
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletarEstudante(@PathVariable Long id){
+        estudanteService.deletarEstudante(id);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{id}")
